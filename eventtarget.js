93f2b5e6f7178dacb8170a861a9bc8e8e8c3cecc
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2013-07-15 11:49:59 +0200

    first version without dispatch

---
+++ eventtarget.js
@@ -0,0 +1,70 @@
+if (!document.addEventListener && window.Element && window.Event) {
+	Event.prototype.stopPropagation = function() { this.cancelBubble = true; }
+	Event.prototype.preventDefault = function() { this.returnValue = false; }
+
+	var proto = {
+		addEventListener: function(type, listener, useCapture) {
+			if (useCapture) { throw new Error("Unable to polyfill event capturing"); }
+
+			if (!this.__events) { this.__events = []; }
+
+			for (var i=0;i<this.__events.length;i++) {
+				var item = this.__events[i];
+				if (item[0] == type && item[1] == listener) { return; }
+			}
+
+			var elm = this;
+			var cb = function(e) {
+				e.timeStamp = +new Date();
+				e.target = e.srcElement;
+				e.currentTarget = elm;
+				e.pageX = e.clientX + document.documentElement.scrollLeft;
+				e.pageY = e.clientY + document.documentElement.scrollTop;
+
+				if (e.type == "mouseover") { 
+					e.relatedTarget = e.fromElement; 
+				} else if (e.type == "mouseout") { 
+					e.relatedTarget = e.toElement; 
+				} else {
+					e.relatedTarget = null;
+				}
+
+				if (typeof(listener) == "function") {
+					listener(e);
+				} else {
+					listener.handleEvent(e);
+				}
+			}
+
+			this.attachEvent("on"+type, cb);
+			this.__events.push([type, listener, cb]);
+		},
+
+		removeEventListener: function(type, listener, useCapture) {
+			if (useCapture) { throw new Error("Unable to polyfill event capturing"); }
+
+			var index = -1;
+			var events = this.__events || [];
+			for (var i=0;i<events.length;i++) {
+				var item = events[i];
+				if (item[0] == type && item[1] == listener) { 
+					index = i;
+					break;
+				}
+			}
+
+			var cb = events.splice(i, 1)[0][2];
+			this.detachEvent("on"+type, cb);
+		},
+
+		dispatchEvent: function(event) {
+			return this.fireEvent("on" + event.type, event);
+		}
+	}
+
+	var todo = [Element, window.constructor, document.constructor];
+	while (todo.length) {
+		var parent = todo.pop();
+		for (var p in proto) { parent.prototype[p] = proto[p]; }
+	}
+}
